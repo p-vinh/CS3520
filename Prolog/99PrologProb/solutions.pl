29a0e24a8211f9f729d4016960e97fa2ef94dec4
random-debug <85326677+random-debug@users.noreply.github.com>
2023-05-03 23:05:59 -0700

    update

--- Prolog/99PrologProb/solutions.pl
+++ Prolog/99PrologProb/solutions.pl
@@ -17,12 +17,21 @@ is_subset([H|T], L) :- is_member(H, L), is_subset(T, L).
 ?- is_union([1,2],[3,4],LR).
 LR = [1,2,3,4]. */
 
-
-is_union(L1, L2, LR) :- append(L1, L2, L), remove_dups(L, LR).
+is_union([], L2, L2).
+is_union([H|T], L2, LR) :-
+    is_member(H, L2),
+    !,
+    is_union(T, L2, LR).
+is_union([H|T], L2, [H|LR]) :-
+    is_union(T, L2, LR).
 
 /* is_intersect(L1,L2,LR), which is true iff LR is the intersection of L1 and L2. (5 points)
 ?- is_intersect([1,2],[2,3],LR).
 LR = [2]. */
+
+is_intersect([], _, []).
+is_intersect([H|T], L2, [H|LR]) :- is_member(H, L2), is_intersect(T, L2, LR).
+is_intersect([_|T], L2, LR) :- is_intersect(T, L2, LR).
 
 /* is_power(L,LR), which is true iff LR is the power set of L. (15 points)
 ?- is_power([1,2],LR).
@@ -32,6 +41,25 @@ LR = [[],[1],[2],[1,2]]. */
 /* Define a relation quicksort(L,SL) which maps a list L to a list SL which is the sorted version of L using the quicksort algorithm. (10 points)
 ?- quicksort([4,2,6,1,3],SL).
 SL = [1,2,3,4,6]. */
+
+
+
+% Base Case
+quicksort([H], [H]).
+quicksort([], []).
+
+quicksort([H|T], SL) :- partition_H([H|T], H, L1, L2), quicksort(L1, LR), quicksort(L2, LR2), append(L1, L2, SL). 
+
+
+partition_H([], _, [], []). 
+partition_H([P|T], P, L, [P|G]) :- partition_H(T, P, L, G).
+partition_H([H|T], P, [H|L], G) :- H =< P, partition_H(T, P, L, G).
+partition_H([H|T], P, L, [H|G]) :- H > P, partition_H(T, P, L, G).
+
+
+list_size([], 0).
+list_size([_|T], S) :- list_size(T, R), S is R + 1.
+
 
 /* Define a relation mergesort(L,SL) which maps a list L to a list SL which is the sorted version of L using the mergesort algorithm. (10 points)
 ?- mergesort([4,2,6,1,3],SL).
